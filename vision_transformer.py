7d957c7ed52133d6e9dd14d1f298124a6d799fcd
5thGenDev <44685200+5thGenDev@users.noreply.github.com>
2023-07-07 17:08:11 +0100

    Update vision_transformer.py

--- vision_transformer.py
+++ vision_transformer.py
@@ -142,6 +142,7 @@ class HydraAttention(nn.Module):
 
 
 # Multi-head Attention
+# For Hydra-Attention that replaces Self-Attention. Set # Head (H) = # Token to get linear computation complexity
 class Block(nn.Module):
     def __init__(self, dim, num_heads, mlp_ratio=4., qkv_bias=False, qk_scale=None, drop=0., attn_drop=0.,
                  drop_path=0., act_layer=nn.GELU, norm_layer=nn.LayerNorm):
