ad205fc48774b6e194523fee76276fe8b94a23cc
5thGenDev <44685200+5thGenDev@users.noreply.github.com>
2023-07-08 16:24:29 +0100

    Update simple_pipeline.py

--- datasets_preprocessing/simple_pipeline.py
+++ datasets_preprocessing/simple_pipeline.py
@@ -35,25 +35,28 @@ def build_transform(
     return transforms.Compose(t)
 
 # now load a train set and a validation set
-def load_dataset(dataset_name):
+def load_dataset(
+    dataset_name, 
+    root="data"
+):
     match dataset_name:
         case "CIFAR10":
-            data = datasets.CIFAR10("data",transform=build_transform(True,224),download=True, )
+            data = datasets.CIFAR10(root,transform=build_transform(True,224),download=True, )
             train_dl = torch.utils.data.DataLoader(data,64,shuffle=True,num_workers=8,pin_memory=True)
             
-            data = datasets.CIFAR10("data",False,transform=build_transform(False,224),download=True)
+            data = datasets.CIFAR10(root,False,transform=build_transform(False,224),download=True)
             val_dl = torch.utils.data.DataLoader(data,64,shuffle=False,num_workers=4)
 
         case "Flowers102":
-            data = datasets.Flowers102("data",transform=build_transform(True,224),download=True, )
+            data = datasets.Flowers102(root,transform=build_transform(True,224),download=True, )
             train_dl = torch.utils.data.DataLoader(data,64,shuffle=True,num_workers=8,pin_memory=True)
             
-            data = datasets.Flowers102("data",False,transform=build_transform(False,224),download=True)
+            data = datasets.Flowers102(root,False,transform=build_transform(False,224),download=True)
             val_dl = torch.utils.data.DataLoader(data,64,shuffle=False,num_workers=4)
 
         case "OxfordIIITPet":
-            data = datasets.OxfordIIITPet("data",transform=build_transform(True,224),download=True, )
+            data = datasets.OxfordIIITPet(root,transform=build_transform(True,224),download=True, )
             train_dl = torch.utils.data.DataLoader(data,64,shuffle=True,num_workers=8,pin_memory=True)
             
-            data = datasets.OxfordIIITPet("data",False,transform=build_transform(False,224),download=True)
+            data = datasets.OxfordIIITPet(root,False,transform=build_transform(False,224),download=True)
             val_dl = torch.utils.data.DataLoader(data,64,shuffle=False,num_workers=4)
