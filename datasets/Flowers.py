6e51044be85ad37feede28734b7ea56a3351134c
5thGenDev <44685200+5thGenDev@users.noreply.github.com>
2023-07-08 14:09:30 +0100

    Update Flowers.py

--- datasets/Flowers.py
+++ datasets/Flowers.py
@@ -1,8 +1,16 @@
 import PIL
-from torchvision import transforms
+from torchvision import transforms as tf
 
 # now load a train set and a validation set
-data = datasets.Flowers102("data",transform=build_transform(True,224),download=True, )
+
+
+data = datasets.Flowers102(
+  "data",
+  transform=build_transform(True,224),
+  download=True, 
+)
+
+
 train_dl = torch.utils.data.DataLoader(data,64,shuffle=True,num_workers=8,pin_memory=True)
 
 data = datasets.Flowers102("data",False,transform=build_transform(False,224),download=True)
