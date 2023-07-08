1f8e0723626337a1d0b04c3f5be3432f1437ebd9
5thGenDev <44685200+5thGenDev@users.noreply.github.com>
2023-07-08 17:28:27 +0100

    Update Attention.py

--- Architectures/For multi-tasks/Attention.py
+++ Architectures/For multi-tasks/Attention.py
@@ -25,3 +25,28 @@ class SelfAttention(nn.Module):
         x = self.proj(x)
         x = self.proj_drop(x)
         return x, attn
+
+# Copied from https://github.com/robflynnyh/hydra-linear-attention/blob/main/hydra.py
+class HydraAttention(nn.Module):
+    def __init__(self, d_model, output_layer='linear', dropout=0.0):
+        super(HydraAttention, self).__init__()
+        self.d_model = d_model
+        self.qkv = nn.Linear(d_model, d_model * 3)
+        self.out = nn.Linear(d_model, d_model) if output_layer == 'linear' else nn.Identity()
+        self.dropout = nn.Dropout(dropout) 
+
+    def forward(self, x, mask=None):
+        '''x: (B, T, D)'''
+        q, k, v = self.qkv(x).chunk(3, dim=-1)
+
+        # Tis cosine similarity kernel instead of softmax
+        q = q / q.norm(dim=-1, keepdim=True) 
+        k = k / k.norm(dim=-1, keepdim=True)
+        
+        if mask is not None:
+            k = k.masked_fill(mask.unsqueeze(-1), 0)
+        kvw = k * v
+        if self.dropout.p > 0:
+            kvw = self.dropout(kvw.transpose(-1, -2)).transpose(-1, -2) # dropout in seq dimension 
+        out = kvw.sum(dim=-2, keepdim=True) * q
+        return self.out(out)
