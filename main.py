fcc2ba46f820cfc155aa0f18e1a1476236e84082
5thGenDev <44685200+5thGenDev@users.noreply.github.com>
2023-07-11 10:23:34 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
+import datetime
+import os
+import os.path as osp
+import sys
+import time
+import warnings
+
+import numpy as np
+import torch
+import torch.backends.cudnn as cudnn
+import torch.nn as nn
+from args import argument_parser, dataset_kwargs, optimizer_kwargs, lr_scheduler_kwargs
+
+import Architectures.vision_transformer as vit
 
