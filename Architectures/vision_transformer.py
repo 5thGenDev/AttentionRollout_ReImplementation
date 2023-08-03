c54a4be2233d1ec54093ed717145da17681bd96b
5thGenDev <44685200+5thGenDev@users.noreply.github.com>
2023-08-03 22:51:13 +0100

    Update vision_transformer.py

--- Architectures/vision_transformer.py
+++ Architectures/vision_transformer.py
@@ -201,30 +201,9 @@ class VisionTransformer(nn.Module):
         return x
 
 
-def vit_tiny(patch_size=16, **kwargs):
-    model = VisionTransformer(
-        patch_size=patch_size, embed_dim=192, depth=12, num_heads=3, mlp_ratio=4,
-        qkv_bias=True, norm_layer=partial(nn.LayerNorm, eps=1e-6), **kwargs)
-    return model
-
-
-def vit_little(patch_size=4, **kwargs):
-    model = VisionTransformer(
-        patch_size=patch_size, embed_dim=64, depth=4, num_heads=4, mlp_ratio=4,
-        qkv_bias=True, norm_layer=partial(nn.LayerNorm, eps=1e-6), **kwargs)
-    return model
-
-
 def vit_small(patch_size=16, **kwargs):
     model = VisionTransformer(
         patch_size=patch_size, embed_dim=384, depth=12, num_heads=6, mlp_ratio=4,
-        qkv_bias=True, norm_layer=partial(nn.LayerNorm, eps=1e-6), **kwargs)
-    return model
-
-
-def vit_base(patch_size=16, **kwargs):
-    model = VisionTransformer(
-        patch_size=patch_size, embed_dim=768, depth=12, num_heads=12, mlp_ratio=4,
         qkv_bias=True, norm_layer=partial(nn.LayerNorm, eps=1e-6), **kwargs)
     return model
 
