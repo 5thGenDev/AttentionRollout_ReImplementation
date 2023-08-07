670e0ad1d514f6906a6aaefc8f14f2147750735d
5thGenDev <44685200+5thGenDev@users.noreply.github.com>
2023-08-07 20:07:56 +0100

    Update simple_pipeline.py

--- datasets_preprocessing/simple_pipeline.py
+++ datasets_preprocessing/simple_pipeline.py
@@ -1,38 +1,5 @@
 import PIL
-from torchvision import datasets, transforms
-
-def build_transform(
-    is_train, 
-    input_size, 
-    mean=(0.485, 0.456, 0.406), 
-    std=(0.229, 0.224, 0.225)
-):
-    # train transform
-    if is_train:
-      # this should always dispatch to transforms_imagenet_train
-      transform = transforms.Compose([
-          transforms.RandomResizedCrop(input_size, scale=(0.2, 1.0), interpolation=3),  # 3 is bicubic
-          transforms.RandomHorizontalFlip(),
-          transforms.ToTensor(),
-          transforms.Normalize(mean=mean, std=std),
-      ])
-      return transform
-    
-    # eval transform
-    t = []
-    if input_size <= 224:
-      crop_pct = 224 / 256
-    else:
-      crop_pct = 1.0
-    size = int(input_size / crop_pct)
-    t.append(
-      transforms.Resize(size, interpolation=PIL.Image.BICUBIC),  # to maintain same ratio w.r.t. 224 images
-    )
-    t.append(transforms.CenterCrop(input_size))
-    
-    t.append(transforms.ToTensor())
-    t.append(transforms.Normalize(mean, std))
-    return transforms.Compose(t)
+from torchvision import datasets
 
 # now load a train set and a validation set
 def load_dataset(
