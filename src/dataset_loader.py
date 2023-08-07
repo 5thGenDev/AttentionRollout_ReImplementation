9c44b370c39b6f0cfcfce79732052d2cfe8e601a
5thGenDev <44685200+5thGenDev@users.noreply.github.com>
2023-08-07 21:22:11 +0100

    Update dataset_loader.py

--- src/dataset_loader.py
+++ src/dataset_loader.py
@@ -2,7 +2,7 @@
 
 import os.path as osp
 
-import PIL
+from PIL import Image
 from torchvision import datasets
 from src.multimodal_transform import Fuse_RGB_Gray_Sketch
 
@@ -14,7 +14,7 @@ def read_image(img_path):
         raise OSError(f"{img_path} does not exist")
     while not got_img:
         try:
-            img = Image.open(img_path).convert("RGB")
+            img = Fuse_RGB_Gray_Sketch(Image.open(img_path))
             got_img = True
         except OSError:
             print(
@@ -23,6 +23,23 @@ def read_image(img_path):
             pass
     return img
 
+
+class ImageDataset(Dataset):
+    """Image Person ReID Dataset"""
+
+    def __init__(self, dataset):
+        self.dataset = dataset
+
+    def __len__(self):
+        return len(self.dataset)
+
+    def __getitem__(self, index):
+        img_path, pid, camid = self.dataset[index]
+        img = read_image(img_path)
+        return img, pid, camid, img_path
+
+
+
 # now load a train set and a validation set
 def load_dataset(
     dataset_name, 
@@ -30,22 +47,22 @@ def load_dataset(
 ):
     match dataset_name:
         case "CIFAR10":
-            data = datasets.CIFAR10(root,transform=build_transform(True,224),download=True, )
+            data = datasets.CIFAR10(root="data",download=True, )
             train_dl = torch.utils.data.DataLoader(dataset=data,batch_size=128,shuffle=True,num_workers=8,pin_memory=True)
             
-            data = datasets.CIFAR10(root,False,transform=build_transform(False,224),download=True)
+            data = datasets.CIFAR10(root="data",False,download=True)
             val_dl = torch.utils.data.DataLoader(dataset=data,batch_size=128,shuffle=False,num_workers=4)
 
         case "Flowers102":
-            data = datasets.Flowers102(root,transform=build_transform(True,224),download=True, )
+            data = datasets.Flowers102(root="data",download=True, )
             train_dl = torch.utils.data.DataLoader(dataset=data,batch_size=128,shuffle=True,num_workers=8,pin_memory=True)
             
-            data = datasets.Flowers102(root,False,transform=build_transform(False,224),download=True)
+            data = datasets.Flowers102(root="data",False,download=True)
             val_dl = torch.utils.data.DataLoader(dataset=data,batch_size=128,shuffle=False,num_workers=4)
 
         case "OxfordIIITPet":
-            data = datasets.OxfordIIITPet(root,transform=build_transform(True,224),download=True, )
-            train_dl = torch.utils.data.DataLoader(dataset=data,batch_size=64,shuffle=True,num_workers=8,pin_memory=True)
+            data = datasets.OxfordIIITPet(root="data",download=True, )
+            train_dl = torch.utils.data.DataLoader(dataset=data,batch_size=128,shuffle=True,num_workers=8,pin_memory=True)
             
-            data = datasets.OxfordIIITPet(root,False,transform=build_transform(False,224),download=True)
-            val_dl = torch.utils.data.DataLoader(dataset=data,batch_size=64,shuffle=False,num_workers=4)
+            data = datasets.OxfordIIITPet(root="data",False,download=True)
+            val_dl = torch.utils.data.DataLoader(dataset=data,batch_size=128,shuffle=False,num_workers=4)
