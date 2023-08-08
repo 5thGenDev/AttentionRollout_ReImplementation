8a8ea4caf4dbca6b75b828c9f8a34bb874edbbf8
5thGenDev <44685200+5thGenDev@users.noreply.github.com>
2023-08-08 14:23:35 +0100

    Update dataset_loader.py

--- src/dataset_loader.py
+++ src/dataset_loader.py
@@ -18,6 +18,29 @@ X, label = training_data[sample_idx]
 matplotlib.pyplot.imshow(X, cmap="gray") where X = PIL_image
 '''
 
+import os
+import sys
+
+from src.pets_flowers import pets
+
+def build_dataset(args, is_train, trnsfrm=None, training_mode='finetune'):
+    if args.data_set == 'Pets':
+        split = 'trainval' if is_train else 'test'
+        dataset = pets(os.path.join(args.data_location, 'Pets_dataset'), split=split, transform=trnsfrm)
+        
+        nb_classes = 37
+
+    else:
+        print('dataloader of {} is not implemented .. please add the dataloader under datasets folder.'.format(args.data_set))
+        sys.exit(1)
+       
+
+        
+        
+    return dataset, nb_classes
+
+
+
 # now load a train set and a validation set
 def load_dataset(
     dataset_name, 
@@ -28,20 +51,9 @@ def load_dataset(
             train_data = datasets.CIFAR10(root=root,train=True,download=True, )
             valid_data = datasets.CIFAR10(root=root,train=False,download=False, )
 
-        case "Flowers102":
-            train_data = datasets.Flowers102(root=root,train=True,download=True, )
-            valid_data = datasets.Flowers102(root=root,train=False,download=True, )
-            
-        case "OxfordIIITPet":
-            data = datasets.OxfordIIITPet(root=root,download=True, )
-            train_data = datasets.CIFAR10(root=root,train=True,download=True, )
-            valid_data = datasets.CIFAR10(root=root,train=False,download=True, )
-
     for sample_idx in range (0, len(training_data)):
         img, label = data[sample_idx] 
         img = Fuse_RGB_Gray_SKetch(img) 
         
     train_dl = torch.utils.data.DataLoader(dataset=data,batch_size=128,shuffle=True,num_workers=8,pin_memory=True)
-    
-    data = datasets.OxfordIIITPet(root=root,train=False,download=True)
     val_dl = torch.utils.data.DataLoader(dataset=data,batch_size=128,shuffle=False,num_workers=4)
