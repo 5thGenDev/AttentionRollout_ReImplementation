98da5e98487c46161c258fbf15714bbed94e1059
5thGenDev <44685200+5thGenDev@users.noreply.github.com>
2023-08-08 12:37:33 +0100

    Update dataset_loader.py

--- src/dataset_loader.py
+++ src/dataset_loader.py
@@ -26,21 +26,18 @@ def load_dataset(
     match dataset_name:
         case "CIFAR10":
             data = datasets.CIFAR10(root="data",download=True, )
-            train_dl = torch.utils.data.DataLoader(dataset=data,batch_size=128,shuffle=True,num_workers=8,pin_memory=True)
-            
-            data = datasets.CIFAR10(root="data",False,download=True)
-            val_dl = torch.utils.data.DataLoader(dataset=data,batch_size=128,shuffle=False,num_workers=4)
 
         case "Flowers102":
             data = datasets.Flowers102(root="data",download=True, )
-            train_dl = torch.utils.data.DataLoader(dataset=data,batch_size=128,shuffle=True,num_workers=8,pin_memory=True)
-            
-            data = datasets.Flowers102(root="data",False,download=True)
-            val_dl = torch.utils.data.DataLoader(dataset=data,batch_size=128,shuffle=False,num_workers=4)
 
         case "OxfordIIITPet":
             data = datasets.OxfordIIITPet(root="data",download=True, )
-            train_dl = torch.utils.data.DataLoader(dataset=data,batch_size=128,shuffle=True,num_workers=8,pin_memory=True)
-            
-            data = datasets.OxfordIIITPet(root="data",False,download=True)
-            val_dl = torch.utils.data.DataLoader(dataset=data,batch_size=128,shuffle=False,num_workers=4)
+
+    for sample_idx in range (0, len(training_data)):
+        img, label = data[sample_idx] 
+        img = Fuse_RGB_Gray_SKetch(img) 
+        
+    train_dl = torch.utils.data.DataLoader(dataset=data,batch_size=128,shuffle=True,num_workers=8,pin_memory=True)
+    
+    data = datasets.OxfordIIITPet(root="data",False,download=True)
+    val_dl = torch.utils.data.DataLoader(dataset=data,batch_size=128,shuffle=False,num_workers=4)
