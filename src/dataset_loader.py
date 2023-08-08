9c806c43bf2abd6afdb2c072198b12c32aadba6f
5thGenDev <44685200+5thGenDev@users.noreply.github.com>
2023-08-08 14:20:45 +0100

    Update dataset_loader.py

--- src/dataset_loader.py
+++ src/dataset_loader.py
@@ -4,7 +4,7 @@ import os.path as osp
 
 from PIL import Image
 from torchvision import datasets
-from src.multimodal_transform import Fuse_RGB_Gray_Sketch
+from src.multimodal_transform import Fuse_RGB_Gray_Sketch, build_test_transform
 
 # img = Fuse_RGB_Gray_SKetch(PIL_image) 
 '''
@@ -21,16 +21,21 @@ matplotlib.pyplot.imshow(X, cmap="gray") where X = PIL_image
 # now load a train set and a validation set
 def load_dataset(
     dataset_name, 
+    root #specify where you download your dataset
 ):
     match dataset_name:
         case "CIFAR10":
-            data = datasets.CIFAR10(root="data",download=True, )
+            train_data = datasets.CIFAR10(root=root,train=True,download=True, )
+            valid_data = datasets.CIFAR10(root=root,train=False,download=False, )
 
         case "Flowers102":
-            data = datasets.Flowers102(root="data",download=True, )
-
+            train_data = datasets.Flowers102(root=root,train=True,download=True, )
+            valid_data = datasets.Flowers102(root=root,train=False,download=True, )
+            
         case "OxfordIIITPet":
-            data = datasets.OxfordIIITPet(root="data",download=True, )
+            data = datasets.OxfordIIITPet(root=root,download=True, )
+            train_data = datasets.CIFAR10(root=root,train=True,download=True, )
+            valid_data = datasets.CIFAR10(root=root,train=False,download=True, )
 
     for sample_idx in range (0, len(training_data)):
         img, label = data[sample_idx] 
@@ -38,5 +43,5 @@ def load_dataset(
         
     train_dl = torch.utils.data.DataLoader(dataset=data,batch_size=128,shuffle=True,num_workers=8,pin_memory=True)
     
-    data = datasets.OxfordIIITPet(root="data",False,download=True)
+    data = datasets.OxfordIIITPet(root=root,train=False,download=True)
     val_dl = torch.utils.data.DataLoader(dataset=data,batch_size=128,shuffle=False,num_workers=4)
