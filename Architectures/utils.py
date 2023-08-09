15aba4a9c5266a353ecf8f7cc72726371edcd5f1
5thGenDev <44685200+5thGenDev@users.noreply.github.com>
2023-08-09 12:34:03 +0100

    Update utils.py

--- Architectures/utils.py
+++ Architectures/utils.py
@@ -71,20 +71,6 @@ def restart_from_checkpoint(ckp_path, run_variables=None, **kwargs):
                 run_variables[var_name] = checkpoint[var_name]
 
 
-def cosine_scheduler(base_value, final_value, epochs, niter_per_ep, warmup_epochs=0, start_warmup_value=0):
-    warmup_schedule = np.array([])
-    warmup_iters = warmup_epochs * niter_per_ep
-    if warmup_epochs > 0:
-        warmup_schedule = np.linspace(start_warmup_value, base_value, warmup_iters)
-
-    iters = np.arange(epochs * niter_per_ep - warmup_iters)
-    schedule = final_value + 0.5 * (base_value - final_value) * (1 + np.cos(np.pi * iters / len(iters)))
-
-    schedule = np.concatenate((warmup_schedule, schedule))
-    assert len(schedule) == epochs * niter_per_ep
-    return schedule
-
-
 def bool_flag(s):
     """
     Parse boolean arguments from the command line.
