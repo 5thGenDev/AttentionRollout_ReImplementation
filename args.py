b71e5cedad82ad992e4af1f49b3c704a5e45d34c
5thGenDev <44685200+5thGenDev@users.noreply.github.com>
2023-08-09 11:34:57 +0100

    Update args.py

--- args.py
+++ args.py
@@ -2,7 +2,7 @@
 
 import argparse
 
-def args_parser():
+def get_args_parser():
     parser = argparse.ArgumentParser('SiT', add_help=False)
 
     # Reconstruction Parameters
@@ -14,6 +14,12 @@ def args_parser():
     
     parser.add_argument('--lmbda', type=int, default=1, help='Scaling factor for the reconstruction loss')
     
+    # SimCLR Parameters
+    parser.add_argument('--out_dim', default=256, type=int, help="Dimensionality of output features")
+    parser.add_argument('--simclr_temp', default=0.2, type=float, help="tempreture for SimCLR.")
+    parser.add_argument('--momentum_teacher', default=0.996, type=float, help="EMA parameter for teacher update.")
+    
+
     # Model parameters
     parser.add_argument('--model', default='vit_small', type=str, choices=['vit_tiny', 'vit_small', 'vit_base'], help="Name of architecture")
     parser.add_argument('--drop_path_rate', type=float, default=0.1, help="stochastic depth rate")
@@ -25,22 +31,26 @@ def args_parser():
     parser.add_argument('--weight_decay_end', type=float, default=0.1)
     parser.add_argument('--clip_grad', type=float, default=3.0)
     
-    parser.add_argument('--batch_size', default=128, type=int)
+    parser.add_argument('--batch_size', default=32, type=int)
     parser.add_argument('--epochs', default=800, type=int, help='Number of epochs of training.')
 
-    parser.add_argument("--lr", default=0.5, type=float, help="Learning rate for SequentialLR. Current Pytorch SOTA")
-    parser.add_argument("--warmup_epochs", default=5, type=int, help="Number of epochs for SequentialLR warm up.")
-    parser.add_argument('--min_lr', type=float, default=1e-6, help="minLR for SequentialLR.")
+    parser.add_argument("--lr", default=0.0005, type=float, help="Learning rate.")
+    parser.add_argument("--warmup_epochs", default=10, type=int, help="Number of epochs for the linear learning-rate warm up.")
+    parser.add_argument('--min_lr', type=float, default=1e-6, help="Target LR at the end of optimization.")
+    
 
-  
     # Dataset
-    parser.add_argument('--data_set', default='Pets', type=str, choices=['CIFAR10', 'Flowers', 'Pets'], help='Name of the dataset.')
+    parser.add_argument('--data_set', default='Pets', type=str, 
+                        choices=['STL10', 'MNIST', 'CIFAR10', 'CIFAR100', 'Flowers', 'Aircraft', 
+                                 'Cars', 'ImageNet5p', 'ImageNet', 'TinyImageNet', 'Pets', 'CUB',
+                                 'PASCALVOC', 'MSCOCO', 'VisualGenome500'], 
+                        help='Name of the dataset.')
     parser.add_argument('--data_location', default='/path/to/dataset', type=str, help='Dataset location.')
 
     parser.add_argument('--output_dir', default="checkpoints/vit_small/trial", type=str, help='Path to save logs and checkpoints.')
-    parser.add_argument('--saveckp_freq', default=5, type=int, help='Save checkpoint every x epochs.')
+    parser.add_argument('--saveckp_freq', default=20, type=int, help='Save checkpoint every x epochs.')
     parser.add_argument('--seed', default=0, type=int, help='Random seed.')
-    parser.add_argument('--num_workers', default=8, type=int, help='Number of data loading workers per GPU.')
+    parser.add_argument('--num_workers', default=10, type=int, help='Number of data loading workers per GPU.')
     parser.add_argument("--dist_url", default="env://", type=str, help="set up distributed training")
     parser.add_argument("--local_rank", default=0, type=int)
     return parser
