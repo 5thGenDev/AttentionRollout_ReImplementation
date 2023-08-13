0a54365cd6125d375d1d78013562cdae46478414
5thGenDev <44685200+5thGenDev@users.noreply.github.com>
2023-08-13 22:12:49 +0100

    Update heatmap.py

--- attention_visualizer/heatmap.py
+++ attention_visualizer/heatmap.py
@@ -1,4 +1,3 @@
-from PIL import Image
 import numpy as np
 import cv2
 
@@ -9,46 +8,3 @@ def show_mask_on_image(img, mask):
     cam = heatmap + np.float32(img)
     cam = cam / np.max(cam)
     return np.uint8(255 * cam)
-
-if __name__ == '__main__':
-    args = get_args()
-    model = torch.hub.load('facebookresearch/deit:main', 
-        'deit_tiny_patch16_224', pretrained=True)
-    model.eval()
-
-    if args.use_cuda:
-        model = model.cuda()
-
-    transform = transforms.Compose([
-        transforms.Resize((224, 224)),
-        transforms.ToTensor(),
-        transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),
-    ])
-    img = Image.open(args.image_path)
-    img = img.resize((224, 224))
-    input_tensor = transform(img).unsqueeze(0)
-    if args.use_cuda:
-        input_tensor = input_tensor.cuda()
-
-    if args.category_index is None:
-        print("Doing Attention Rollout")
-        attention_rollout = VITAttentionRollout(model, head_fusion=args.head_fusion, 
-            discard_ratio=args.discard_ratio)
-        mask = attention_rollout(input_tensor)
-        name = "attention_rollout_{:.3f}_{}.png".format(args.discard_ratio, args.head_fusion)
-    else:
-        print("Doing Gradient Attention Rollout")
-        grad_rollout = VITAttentionGradRollout(model, discard_ratio=args.discard_ratio)
-        mask = grad_rollout(input_tensor, args.category_index)
-        name = "grad_rollout_{}_{:.3f}_{}.png".format(args.category_index,
-            args.discard_ratio, args.head_fusion)
-
-
-    np_img = np.array(img)[:, :, ::-1]
-    mask = cv2.resize(mask, (np_img.shape[1], np_img.shape[0]))
-    mask = show_mask_on_image(np_img, mask)
-    cv2.imshow("Input Image", np_img)
-    cv2.imshow(name, mask)
-    cv2.imwrite("input.png", np_img)
-    cv2.imwrite(name, mask)
-    cv2.waitKey(-1)
