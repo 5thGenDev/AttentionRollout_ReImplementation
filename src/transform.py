acb60dc50ee3c955357aa91b3f8712b2693f83a4
5thGenDev <44685200+5thGenDev@users.noreply.github.com>
2023-08-13 22:57:33 +0100

    Update transform.py

--- src/transform.py
+++ src/transform.py
@@ -1,38 +1,26 @@
 from torchvision import datasets, transforms
 
-# Input_size = 256
-transform = transforms.Compose([
-    transforms.Resize(size, interpolation=3),
-    transforms.CenterCrop(input_size),
-    transforms.ToTensor(),
-    transforms.Normalize(ImageNet_mean, ImageNet_std)])
-
-# Input_size = 256
-def build_transform(is_train, input_size):
+# input_size = desired size
+def build_transform(is_train, input_size=224):
     ImageNet_mean = (0.485, 0.456, 0.406)
     ImageNet_std = (0.229, 0.224, 0.225)
 
-    if input_size <= 224:
-        crop_pct = 224 / 256
-    else:
-        crop_pct = 1.0
-    size = int(input_size / crop_pct)
-    
     # train transform
     if is_train:
         transform = transforms.Compose([
             # 3 = PIL.Image.BICUBIC
-            transforms.RandomResizedCrop(size, scale=(0.2, 1.0), interpolation=3), 
+            transforms.RandomResizedCrop(input_size, scale=(0.2, 1.0), interpolation=3), 
             transforms.RandomHorizontalFlip(),
             transforms.ToTensor(),
-            transforms.Normalize(ImageNet_mean, ImageNet_mean)])
+            transforms.Normalize(ImageNet_mean, ImageNet_mean),
+        ])
         return transform
 
     # eval transform
     transform = transforms.Compose([
-        # for input_size = 256, size = 256
-        transforms.Resize(size, interpolation=3),
-        transforms.CenterCrop(224),
+        transforms.Resize(256, interpolation=3),
+        transforms.CenterCrop(input_size),
         transforms.ToTensor(),
-        transforms.Normalize(ImageNet_mean, ImageNet_std)])
+        transforms.Normalize(ImageNet_mean, ImageNet_std),
+    ])
     return transform
