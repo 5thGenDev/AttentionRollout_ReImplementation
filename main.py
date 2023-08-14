3228eec63f13a188ff057e3eaaf99aa91f31f6ce
5thGenDev <44685200+5thGenDev@users.noreply.github.com>
2023-08-14 21:21:16 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from torchvision import transforms
 import numpy as np
 import cv2
 
+from Architectures.vit import vit_base_16, vit_base_32, vit_huge_14
+
 from attention_visualizer.heatmap import show_mask_on_image
 from attention_visualizer.hook_rollout import Hook
 
