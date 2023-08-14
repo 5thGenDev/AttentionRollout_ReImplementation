a4ca1497c266638e9fec00874b20158391b744d6
5thGenDev <44685200+5thGenDev@users.noreply.github.com>
2023-08-14 21:45:46 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,11 +4,12 @@ import torch
 
 from Architectures import vit 
 from src.load_dataset import build_dataset
+from src.transform import build_transform
 
 if __name__ == '__main__':
     set_random_seed(args.seed)
     args = get_args()
 
-    
+    data_loader, classes = build_dataset(args, is_train=True, trnsfrm=transform)
     model = vits.__dict__[args.model](img_size=224, num_classes=classes)
    
