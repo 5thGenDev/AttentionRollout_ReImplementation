be2871da904b34c9e048a4fa111d752f08b250b5
5thGenDev <44685200+5thGenDev@users.noreply.github.com>
2023-08-14 21:09:34 +0100

    Update main.py

--- main.py
+++ main.py
@@ -12,34 +12,4 @@ from attention_visualizer.hook_rollout import Hook
 if __name__ == '__main__':
     set_random_seed(args.seed)
     args = get_args()
-    model = torch.hub.load('facebookresearch/deit:main', 
-        'deit_tiny_patch16_224', pretrained=True)
-    model.eval()
-
-    if args.use_cuda:
-        model = model.cuda()
-
-    transform = transforms.Compose([
-        transforms.Resize((224, 224)),
-        transforms.ToTensor(),
-        transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),
-    ])
-    img = Image.open(args.image_path)
-    img = img.resize((224, 224))
-    input_tensor = transform(img).unsqueeze(0)
-    if args.use_cuda:
-        input_tensor = input_tensor.cuda()
-
-    print("Doing Attention Rollout")
-    attention_rollout = Hook(model, head_fusion=args.head_fusion)
-    mask = attention_rollout(input_tensor)
-    name = "attention_rollout_{}.png".format(args.head_fusion)
-
-    np_img = np.array(img)[:, :, ::-1]
-    mask = cv2.resize(mask, (np_img.shape[1], np_img.shape[0]))
-    mask = show_mask_on_image(np_img, mask)
-    cv2.imshow("Input Image", np_img)
-    cv2.imshow(name, mask)
-    cv2.imwrite("input.png", np_img)
-    cv2.imwrite(name, mask)
-    cv2.waitKey(-1)
+   
