37a496e04dff6fffefa917bbc06251615eef16e0
5thGenDev <44685200+5thGenDev@users.noreply.github.com>
2023-08-14 21:42:45 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,13 @@
 import argparse
 import sys
 import torch
-from PIL import Image
-from torchvision import transforms
-import numpy as np
-import cv2
 
-from Architectures.vit import vit_base_16, vit_base_32, vit_huge_14
-
-from attention_visualizer.heatmap import show_mask_on_image
-from attention_visualizer.hook_rollout import Hook
+from Architectures import vit 
 
 if __name__ == '__main__':
     set_random_seed(args.seed)
     args = get_args()
+
+    
+    model = vits.__dict__[args.model](img_size=224, num_classes=classes)
    
