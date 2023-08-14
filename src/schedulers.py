6cd3061d1f54c7edd286d2dd5b57bf0f8cd09183
5thGenDev <44685200+5thGenDev@users.noreply.github.com>
2023-08-15 00:06:54 +0100

    Update schedulers.py

--- src/schedulers.py
+++ src/schedulers.py
@@ -8,15 +8,16 @@ def init_lr_scheduler(
     epochs,
     stepsize=[20, 40],  # step size to decay learning rate
     gamma=0.1,  # learning rate decay
+    warmup_epochs=5,
+    min_lr=1e-6,    
 ):
     '''
-    Read this line of code: https://github.com/pytorch/vision/blob/main/references/classification/train.py#L304
+    Read https://github.com/pytorch/vision/blob/main/references/classification/train.py#L304
     and: https://pytorch.org/blog/how-to-train-state-of-the-art-models-using-torchvision-latest-primitives/#lr-optimizations
     '''
-    lr_warmup_epochs = 5
     lr_warmup_decay = 0.01
     warmup_lr_scheduler = torch.optim.lr_scheduler.LinearLR(
-        optimizer, start_factor=lr_warmup_decay, total_iters=lr_warmup_epochs
+        optimizer, start_factor=lr_warmup_decay, total_iters=warmup_epochs
     )
 
     '''
@@ -24,7 +25,7 @@ def init_lr_scheduler(
     and: https://pytorch.org/blog/how-to-train-state-of-the-art-models-using-torchvision-latest-primitives/#:~:text=decaying%20the%20LR%20up%20to%20zero
     '''
     main_lr_scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(
-        optimizer, T_max=epochs - lr_warmup_epochs, eta_min=0
+        optimizer, T_max=epochs - warmup_epochs, eta_min=min_lr
     )
     
     if lr_scheduler == "single_step":
@@ -39,7 +40,7 @@ def init_lr_scheduler(
 
     elif lr_scheduler == "sequential":
         return torch.optim.lr_scheduler.SequentialLR(
-            optimizer, schedulers=[warmup_lr_scheduler, main_lr_scheduler], milestones=[lr_warmup_epochs]
+            optimizer, schedulers=[warmup_lr_scheduler, main_lr_scheduler], milestones=[warmup_epochs]
         )
     else:
         raise ValueError(f"Unsupported lr_scheduler: {lr_scheduler}")
