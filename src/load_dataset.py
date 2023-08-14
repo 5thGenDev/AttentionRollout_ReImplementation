10b3eafd989bb4c0d08c72d672517febc5bea2f7
5thGenDev <44685200+5thGenDev@users.noreply.github.com>
2023-08-14 14:38:31 +0100

    Update load_dataset.py

--- src/load_dataset.py
+++ src/load_dataset.py
@@ -12,17 +12,21 @@ def build_dataset(args, is_train, trnsfrm=None):
         split = 'trainval' if is_train else 'test'
         root = os.path.join(args.data_location, folder)
         dataset = D.Flowers102(root=root, split=split, transform=trnsfrm, download=True,)
+        classes = 37
 
     if agrs.data_set == 'Flowers':
         folder = 'Oxford102Flowers'
         split = 'train' if is_train else 'test'
         root = os.path.join(args.data_location, folder)
         dataset = D.Flowers102(root=root, split=split, transform=trnsfrm, download=True,)
+        classes = 102
 
     if args.data_Set == 'CIFAR10':
         folder = 'CIFAR10'
         root = os.path.join(args.data_location, folder)
         dataset = D.CIFAR10(root=root, transform=trnsfrm, download=True,)
+        classes = 10
+        
     else:
         print('dataloader of {} is not implemented .. please add the dataloader under datasets folder.'.format(args.data_set))
         sys.exit(1)
@@ -30,5 +34,5 @@ def build_dataset(args, is_train, trnsfrm=None):
     data_loader = torch.utils.data.DataLoader(dataset, batch_size=args.batch_size, num_workers=args.num_workers, pin_memory=True, drop_last=True)
     print(f"Data loaded: there are {len(dataset)} images.")
     
-    return data_loader
+    return data_loader, classes
 
