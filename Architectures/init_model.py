8ee956655a27892c694bebb5daa69e57b2227e71
5thGenDev <44685200+5thGenDev@users.noreply.github.com>
2023-08-15 19:54:52 +0100

    Create init_model.py

---
+++ Architectures/init_model.py
@@ -0,0 +1,20 @@
+import torch.utils.model_zoo as model_zoo
+import torchvision
+
+def init_pretrained_weights(model, model_url):
+    """
+    Initialize model with pretrained weights.
+    Layers that don't match with pretrained layers in name or size are kept unchanged.
+    """
+    pretrain_dict = model_zoo.load_url(model_url)
+    model_dict = model.state_dict()
+    pretrain_dict = {
+        k: v
+        for k, v in pretrain_dict.items()
+        if k in model_dict and model_dict[k].size() == v.size()
+    }
+    model_dict.update(pretrain_dict)
+    for param_tensor in model_dict:
+        print(param_tensor, "\t", model_dict[param_tensor].size())
+    model.load_state_dict(model_dict)
+    print(f"Initialized model with pretrained weights from {model_url}")
