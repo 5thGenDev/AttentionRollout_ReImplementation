8c3b3e70261521c10babf8c5ba889b96524c82f5
5thGenDev <44685200+5thGenDev@users.noreply.github.com>
2023-08-18 12:08:08 +0100

    Update heatmap.py

--- attention_visualizer/heatmap.py
+++ attention_visualizer/heatmap.py
@@ -8,4 +8,19 @@ def show_mask_on_image(img, mask):
     heatmap = cv2.cvtColor(heatmap, cv2.COLOR_BGR2RGB)
     cam = heatmap + np.float32(img)
     cam = cam / np.max(cam)
+
+    # Making bigger seperation of values in heatmap
+    heatmapshow = None
+    heatmapshow = cv2.normalize(cam, heatmapshow, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8U)
+    heatmapshow = cv2.applyColorMap(heatmapshow, cv2.COLORMAP_JET)
+
+    # Increasing contrast
+    clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
+    channels = cv2.split(heatmapshow)
+    clahe_channels = [clahe.apply(c) for c in channels]
+    heatmapshow = cv2.merge(clahe_channels)
+
+    # Decreasing noise
+    heatmapshow = cv2.GaussianBlur(heatmapshow, (5, 5), 0)
+
     return np.uint8(255 * cam)
